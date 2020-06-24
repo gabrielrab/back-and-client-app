5b1bd72b790a3a1f826937f41a3122409aaa0eb6
Gabriel Rabelo Camargos <contato.ideialisa@gmail.com>
2020-06-23 22:40:35 -0300

    create client

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+export default function App() {
+  const [toggle, setToggle] = useState(false);
+  const [result, setResult] = useState(false);
+
+  const callApi = async () => {
+    const data = await axios.get('http://localhost:3000/');
+    setResult(data.data);
+    setToggle(true);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a project with the frontend and the backend attached
+        </p>
+        <button className="btn" onClick={callApi}>
+          Click here and call backend !
+        </button>
+        {!toggle ? (
+          <></>
+        ) : (
+          <>
+            <div className="result-api">
+              <code>{result}</code>
+            </div>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
